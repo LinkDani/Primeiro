8019dbecf50b8a201a4f0a4f905fcca5d72c3ef4
Daniel Walter <danielwalterpand@gmail.com>
2017-04-04 21:12:14 -0300

    Refatoração no cálculo da soma

--- calc.js
+++ calc.js
@@ -1,13 +1,18 @@
+
 'use strict';
 
-function soma(a, b) {
-  if(!a || !b)
-  return {message: 'parâmetros faltando'};
+function soma(a, b = 0) {
+  if (a === undefined)
+    return { message: 'parâmentos faltando'};
 
   if (typeof a === 'string' || typeof b === 'string')
-return { message: 'parâmentos inválidos'};
+    return { message: 'parâmentos inválidos'};
 
-  return a + b;
+  let resultado = 0;
+  for(let i = 0; i < arguments.length; i++) {
+    resultado = resultado + arguments[i];
+  }
+  return resultado;
 }
 
 module.exports = { soma };
